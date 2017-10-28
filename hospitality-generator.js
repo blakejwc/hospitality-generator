dd99dfb20c1fbbeb9162e2117641953698ef92e4
blake.chambers <blake.chambers@vecna.com>
2017-10-27 23:31:20 -0400

    Updating code to be better suited for squarespace
    Shout out to https://github.com/mattWillis for his help making
    this responsive on squarespace!

--- hospitality-generator.js
+++ hospitality-generator.js
@@ -1,61 +1,77 @@
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType('application/json');
-    xobj.open('GET', 'challengs.json', true);
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState === 4 && xobj.status === '200') {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);
-}
-
-function displayChallenge(newChallenge, newVerse) {
-    var timeAnimation = 500;
-    var challengeContainer = $('#challengeContainer');
-    //fade out animation with callback
-    challengeContainer.fadeOut(timeAnimation, function() {
-        challengeContainer.html('');
-        challengeContainer.append('<p>' + newChallenge + '</p>'
-            + '<p id="challengeGenius">' + newVerse + '</p>');
-
-        //fadein animation.
-        challengeContainer.fadeIn(timeAnimation);
-    });
-}
-
-function buttonClickHandler(challengeList) {
-    //getting a new random number to attach to a challenge and setting a limit
-    var listLength = challengeList.length;
-    var randomNumber = Math.floor(Math.random() * listLength);
-    //set a new challenge
-    var newChallenge = challengeList[randomNumber].challenge;
-    var newVerse = challengeList[randomNumber].verse;
-    //console.log(newChallengeText,newChallengeGenius);
-    displayChallenge(newChallenge, newVerse);
-}
-
 $(document).ready(function() {
-    var challengeSource;
-    loadJSON(function(response) {
-        challengeSource = JSON.parse(response);
+    function loadJSON(jsonFile, callback) {
+
+        var xobj = new XMLHttpRequest();
+        xobj.overrideMimeType('application/json');
+        xobj.open('GET', jsonFile, true);
+        xobj.onreadystatechange = function () {
+            if (xobj.readyState === 4 && xobj.status === 200) {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                console.log(JSON.parse(xobj.responseText));
+            }
+        };
+        xobj.send(null);
+    }
+
+    function displayChallenge(newChallenge) {
+        var timeAnimation = 500;
+        var challengeContainer = $('#hr_generator_challenges');
+        //fade out animation with callback
+        challengeContainer.fadeOut(timeAnimation, function() {
+            challengeContainer.html('<h3 class="hr_generator_challenge">' + newChallenge + '</h3>');
+
+            //fadein animation.
+            challengeContainer.fadeIn(timeAnimation);
+        });
+    }
+
+    function buttonClickHandler(challengeList) {
+        //getting a new random number to attach to a challenge and setting a limit
+        var listLength = challengeList.length;
+        var randomNumber = Math.floor(Math.random() * listLength);
+        //set a new challenge
+        var newChallenge = challengeList[randomNumber];
+        displayChallenge(newChallenge);
+    }
+
+    var challengeSrc = 'https://raw.githubusercontent.com/blakejwc/hospitality-generator/master';
+
+    var easyChallenge;
+    $.getJSON(challengeSrc + '/easy-challenges.json', function (response) {
+        easyChallenge = response;
     });
-    $('#easyChallengeButton').click(function(evt) {
+    var mediumChallenge;
+    $.getJSON(challengeSrc + '/medium-challenges.json', function (response) {
+        mediumChallenge = response;
+    });
+    var hardChallenge;
+    $.getJSON(challengeSrc + '/hard-challenges.json', function (response) {
+        hardChallenge = response;
+    });
+    // Doesn't work on squarespace :(
+    // loadJSON('easy-challenges.json', function(response) {
+    //     easyChallenge = JSON.parse(response);
+    // });
+    // loadJSON('medium-challenges.json', function(response) {
+    //     mediumChallenge = JSON.parse(response);
+    // });
+    // loadJSON('hard-challenges.json', function(response) {
+    //     hardChallenge = JSON.parse(response);
+    // });
+    $('#hr_generator_button-easy').click(function(evt) {
         //prevent browser's default action
         evt.preventDefault();
-        buttonClickHandler(challengeSource.easy);
+        buttonClickHandler(easyChallenge);
     });
-    $('#mediumChallengeButton').click(function(evt) {
+    $('#hr_generator_button-medium').click(function(evt) {
         //prevent browser's default action
         evt.preventDefault();
-        buttonClickHandler(challengeSource.medium);
+        buttonClickHandler(mediumChallenge);
     });
-    $('#hardChallengeButton').click(function(evt) {
+    $('#hr_generator_button-hard').click(function(evt) {
         //prevent browser's default action
         evt.preventDefault();
-        buttonClickHandler(challengeSource.hard);
+        buttonClickHandler(hardChallenge);
     });
-});//end document ready
+});
 
